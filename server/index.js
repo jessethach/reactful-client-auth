66fa48ef99cb5ebb23d02b84aad2e605ce7d2ea3
jessethach <jessethach@gmail.com>
2016-04-26 12:21:52 -0700

    developed authentication in react using best practices

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require(__dirname + '/router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
